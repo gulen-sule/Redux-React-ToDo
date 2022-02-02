7076bd5cdeb598cbb581c61f7365f70e9a2b285d
gulen-sule <gulensule@gmail.com>
2022-02-02 13:47:30 +0300

    02.02.2022 error fixed

--- src/constants/ActionTypes.js
+++
@@ -1,5 +0,0 @@
-export const ADD_LIST='ADD_LIST'
-export const ADD_TODO='ADD_TODO'
-export const DELETE_LIST='DELETE_LIST'
-export const DELETE_TODO='DELETE_TODO'
-export const EDIT_TODO='EDIT_TODO'
--- src/redux/reducers/profileReducer.js
+++ src/redux/reducers/profileReducer.js
@@ -1,4 +1,4 @@
-import *  as types from '../../constants/ActionTypes'
+import *  as types from '../../constants/actionTypes'
 
 export default function profileReducer(state, action) {
     switch (action.type) {
